963ca3733c85474c11f83fa31a60d704aed87c76
ERAY <ERAY@DESKTOP-9JI43N4>
2022-09-28 20:51:43 +0300

    .

---
+++ odev2.2.4/src/CustomerManager.java
@@ -0,0 +1,17 @@
+
+public class CustomerManager { // class PascalCase
+	//classlar reference type
+	public void add() // method camelCase
+	{
+		System.out.println("müşteri  eklendi");
+	}
+
+	public void remove() {
+		System.out.println("müşteri  silindi");
+	}
+
+	public void update() {
+		System.out.println("müşteri  güncellendi");
+	}
+
+}
