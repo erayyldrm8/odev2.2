76ec0fa368c0f9af0bee5f7f37ba1852afe4366a
ERAY <ERAY@DESKTOP-9JI43N4>
2022-09-29 12:47:33 +0300

    .

--- odev2.2.4/src/Main.java
+++ odev2.2.4/src/Main.java
@@ -3,13 +3,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager;// = new CustomerManager();
 		CustomerManager customerManager2 = new CustomerManager();
-		customerManager=customerManager2;
-		
+		customerManager = customerManager2;
+
 		customerManager.add();
 		customerManager.remove();
 		customerManager.update();
+
+		// value type
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi2 = sayi1;
+		sayi1 = 30;
+		System.out.println(sayi2);
+
+		int[] sayilar = new int[] { 1, 2, 3 };
 	}
 
 }
